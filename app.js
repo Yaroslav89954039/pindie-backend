a394f290efbfd9f20a166dd6d2b325a96c0455a6
Yaroslav89954039 <149409830+Yaroslav89954039@users.noreply.github.com>
2024-05-15 14:16:01 +0700

    create express app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path")
+const bodyParser = require("body-parser");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+const usersRouter = require("./routes/users");
+const app = express();
+
+const PORT = 3000;
+app.use(
+bodyParser.json,
+express.static(path.join(__dirname, "/public")),
+gamesRouter,
+categoriesRouter,
+usersRouter
+);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
